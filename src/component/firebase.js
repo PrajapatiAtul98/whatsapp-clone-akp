627ca3fcfa7b8045f89b8698aee8663b1196c9f0
Atul Prajapati <atulkumarprajapati5698@gmail.com>
2023-02-18 14:11:45 +0530

    ss

--- src/component/firebase.js
+++ src/component/firebase.js
@@ -4,6 +4,14 @@ import "firebase/compat/firestore";
 
 
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAx-CXwjko0ZgkJwmMD0IBMnNrQo_CNXH0",
+//   authDomain: "whatsapp-clone-akp.firebaseapp.com",
+//   projectId: "whatsapp-clone-akp",
+//   storageBucket: "whatsapp-clone-akp.appspot.com",
+//   messagingSenderId: "561118535481",
+//   appId: "1:561118535481:web:189020fd23ba074a8e92e8"
+// };
 const firebaseConfig = {
   apiKey: "AIzaSyAx-CXwjko0ZgkJwmMD0IBMnNrQo_CNXH0",
   authDomain: "whatsapp-clone-akp.firebaseapp.com",
@@ -12,7 +20,6 @@ const firebaseConfig = {
   messagingSenderId: "561118535481",
   appId: "1:561118535481:web:189020fd23ba074a8e92e8"
 };
-
 
 //this will connect everything
 const firebaseApp = firebase.initializeApp(firebaseConfig); 
