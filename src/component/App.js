146cd530d7c1d9d5fbef57f54049048438b90783
Atul Prajapati <atulkumarprajapati5698@gmail.com>
2023-06-22 20:14:41 +0530

    nn

--- src/component/App.js
+++ src/component/App.js
@@ -1,45 +1,43 @@
-import React from 'react';
-import './App.css';
-import { Sidebar } from './Sidebar';
-import { Chatbar } from './Chatbar';
-import { Route } from 'react-router-dom';
-import { Routes } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
-import Login from './Login';
-import { useStateValue } from './StateProvider';
-import { auth } from './firebase';
-import { useEffect } from 'react';
-import { actionTypes } from './reudcer';
-
+import React from "react";
+import "./App.css";
+import { Sidebar } from "./Sidebar";
+import { Chatbar } from "./Chatbar";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
+import { useEffect } from "react";
+import { actionTypes } from "./reudcer";
 
 function App() {
-  const [{ user },dispatch] = useStateValue();
-    console.log("user",user)
+  const [{ user }, dispatch] = useStateValue();
+  console.log("user", user);
   useEffect(() => {
-    auth.onAuthStateChanged(user => {
+    auth.onAuthStateChanged((user) => {
       dispatch({
         type: actionTypes.SET_USER,
-        user: user
-      })
-    })
-  }, [])
+        user: user,
+      });
+    });
+  }, []);
   return (
     <BrowserRouter>
-
-      {!user ? <Login /> : (
-        <div id="main" className='app'>
-          <div className='app-body'>
+      {!user ? (
+        <Login />
+      ) : (
+        <div id="main" className="app">
+          <div className="app-body">
             <Sidebar />
             <Routes>
-              <Route exact path="/" element={< Chatbar />} />
-              <Route path="/room/:roomId" element={< Chatbar />} />
+              <Route exact path="/" element={<Chatbar />} />
+              <Route path="/room/:roomId" element={<Chatbar />} />
             </Routes>
           </div>
         </div>
       )}
-
     </BrowserRouter>
-
   );
 }
 
